34346c9ccb3f87284b2103a2e88fd52cc7e78a57
mccutchen <mccutchen@081b0654-6732-0410-aa48-3f6d4c21b6a6>
2007-07-25 02:57:04 +0000

    preprocess.py will now try to get the input path from the command line
    git-svn-id: https://bhc.googlecode.com/svn/schedulexml-preprocessors/trunk@124 081b0654-6732-0410-aa48-3f6d4c21b6a6

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os, shutil, sys
 
-# ===================================================================
+# ==============================================================================
 # Settings
-# ===================================================================
+# ==============================================================================
 
 # Where are the preprocessors located (no trailing slash)
 PREPROCESSOR_PATH = "preprocessors"
@@ -13,33 +13,42 @@ PREPROCESSOR_PATH = "preprocessors"
 PREPROCESSOR_ORDER = "%s/ORDER.txt" % PREPROCESSOR_PATH
 
 # Where is Saxon located?
-SAXON_PATH = os.name == 'posix' and "~/src/saxon/saxon8.jar" or "C:/saxon/saxon8.jar"
+SAXON_PATH = os.name == 'posix' \
+    and "~/src/saxon/saxon8.jar" \
+    or "C:/saxon/saxon8.jar"
 
 # Where should the output go?
 SCHEDULE_FILE = "schedule.xml"
 
-# Where is Rik's input file?
-INPUT_PATH = "input/schedule-200-2007SP.xml"
+# The default ScheduleXMl file to preprocess
+DEFAULT_INPUT_PATH = "input/schedule-200-2007SP.xml"
+
+# Try to get an input path from the command line
+try:
+    INPUT_PATH = sys.argv[1]
+    assert os.path.exists(INPUT_PATH)
+except (IndexError, AssertionError):
+    INPUT_PATH = DEFAULT_INPUT_PATH
 
 
-# ===================================================================
+# ============================================================================
 # Run
-# ===================================================================
+# ============================================================================
 print "Preprocessing Rik's ScheduleXML...\n"
 
 # First, copy Rik's original schedule to a new location
 print "Copying input..."
 shutil.copy(INPUT_PATH, SCHEDULE_FILE)
-print
+print "%s => %s\n" % (INPUT_PATH, SCHEDULE_FILE)
 
-# Loop through each of the preprocessors defined in the file found
-# at $PREPROCESSOR_ORDER
+# Loop through each of the preprocessors defined in the file found at
+# PREPROCESSOR_ORDER
 print "Running preprocessors..."
 for p in file(PREPROCESSOR_ORDER):
     p = p.strip()
     print " - %s.xsl" % p
-    #cmd = 'java -jar $SAXON_PATH -o $SCHEDULE_FILE $SCHEDULE_FILE $PREPROCESSOR_PATH/$p.xsl'
-    cmd = 'java -jar %s -o %s %s %s/%s.xsl' % (SAXON_PATH, SCHEDULE_FILE, SCHEDULE_FILE, PREPROCESSOR_PATH, p)
+    cmd = 'java -jar %s -o %s %s %s/%s.xsl' % \
+        (SAXON_PATH, SCHEDULE_FILE, SCHEDULE_FILE, PREPROCESSOR_PATH, p)
     os.system(cmd)
 print "Finished.\n"
 
