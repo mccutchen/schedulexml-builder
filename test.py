624faa76d26982433bd81e3713841c87dd087be2
mccutchen <mccutchen@081b0654-6732-0410-aa48-3f6d4c21b6a6>
2008-04-14 21:46:08 +0000

    Improved formatting
    git-svn-id: https://bhc.googlecode.com/svn/schedulexml-preprocessors/trunk@436 081b0654-6732-0410-aa48-3f6d4c21b6a6

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 """test.py -- crude tests for the ScheduleXML preprocessors
 
-This is a really crude test for consistency between the input ScheduleXML
-document from Rik Jones and the output ScheduleXML file after being
-preprocessed.
+This is a really crude test for consistency between the input
+ScheduleXML document from Rik Jones and the output ScheduleXML file
+after being preprocessed.
 
-At the moment, this just ensures that the root elements are the same, that
-a few elements are included in the output file, and most importantly, that
-the number of <class> elements is identical.
+At the moment, this just ensures that the root elements are the same,
+that a few elements are included in the output file, and most
+importantly, that the number of <class> elements is identical.
 
-Making sure the number of <class> elements is identical tries to ensure that
-we're not dropping information."""
+Making sure the number of <class> elements is identical tries to
+ensure that we're not dropping information."""
 
 try:
     # Python 2.5 has ElementTree built in
@@ -27,13 +27,16 @@ def test(old, new):
     new = ET.ElementTree(file=new)
 
     # make sure we have the correct root element
-    assert old.getroot().tag == MUST_BE_ROOT and new.getroot().tag == MUST_BE_ROOT, 'Wrong root element.  Expected <%s>' % MUST_BE_ROOT
+    assert old.getroot().tag == MUST_BE_ROOT and \
+        new.getroot().tag == MUST_BE_ROOT, \
+        'Wrong root element.  Expected <%s>' % MUST_BE_ROOT
 
     # make sure each element that must be in the schedule
     # is actually there
     for tag in MUST_EXIST:
         xpath = '//%s' % tag
-        assert old.findall(xpath) and new.findall(xpath), 'Element <%s> missing.' % tag
+        assert old.findall(xpath) and new.findall(xpath), \
+            'Element <%s> missing.' % tag
 
     # make sure that each element which must show up an
     # equal amount of times, does
@@ -42,14 +45,18 @@ def test(old, new):
         old_els = old.findall(xpath)
         new_els = new.findall(xpath)
 
-        assert len(old_els) == len(new_els), 'Element <%s> does not appear the same number of times (%d in old document vs. %d in new document).' % (tag, len(old_els), len(new_els))
+        assert len(old_els) == len(new_els), \
+            'Element <%s> does not appear the same number of times ' \
+            '(%d in old document vs. %d in new document).' \
+            % (tag, len(old_els), len(new_els))
     
     # all tests passed
     return True
 
 if __name__ == '__main__':
     import sys
-    assert len(sys.argv) == 3, 'The two files to compare must be given as arguments'
+    assert len(sys.argv) == 3, \
+        'The two files to compare must be given as arguments'
     a, b = sys.argv[1:]
     try:
         test(a, b)
